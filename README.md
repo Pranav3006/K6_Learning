ab98e10ed0442bac2e1915fbcdec6bf299e9ddbb
Pranav3006 <pranavwork3006@gmail.com>
2026-05-12 16:18:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# K6 Learning Journey
+
+This repository contains my learning and practice scripts for Grafana k6 performance testing.
+
+## Topics Covered
+- Basic GET requests
+- Checks & validations
+- Load testing
+- Thresholds
+- Scenarios
+- Kubernetes operator
+- Grafana integration
---
+++ first_Script.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  vus: 50,
+  duration: '10s',
+};
+
+export default function () {
+  const res = http.get('https://test.k6.io');
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
